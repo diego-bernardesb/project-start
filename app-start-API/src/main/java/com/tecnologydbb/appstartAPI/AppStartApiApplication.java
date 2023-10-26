069586683e925bb58618c7967f320e7ba3d428c7
Diego Bernardes Babo <diegobbabo@gmail.com>
2023-10-26 18:03:33 -0300

    add back-end

---
+++ app-start-API/src/main/java/com/tecnologydbb/appstartAPI/AppStartApiApplication.java
@@ -0,0 +1,13 @@
+package com.tecnologydbb.appstartAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStartApiApplication.class, args);
+	}
+
+}
---
+++ app-start-API/src/test/java/com/tecnologydbb/appstartAPI/AppStartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnologydbb.appstartAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppStartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
